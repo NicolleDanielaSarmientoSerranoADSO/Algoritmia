32a36f3aa7c81d8aa87b8a2b1c8065d0347aae8b
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-12-06 09:14:05 -0500

    Sección5

--- Sección1/main.js
+++ Sección1/main.js
@@ -1,16 +1,19 @@
 addEventListener("DOMContentLoaded", (e) =>{
-    form.addEventListener("submit",(e)=>{
+    form=document.querySelector("#form")
+        let form=document.querySelector("#form")
+    
+        form.addEventListener("submit",(e)=>{
         e.preventDefault();
-        let data= Object.fromEntries(new FormData(e.tarjet));
-        let gradosc=document.querySelector("gradosc")
-        let celcius= Number(data.gradosc)
-        let farenheit=(gradosc-32)*5/9
-        let mytable=document.querySelector("tbody");
-        mytable.insertAdjacentHTML("beforeend",`
-        <tr>
-            <td>${celcius}</td>
-            <td>${farenheit}</td>
-        </tr>
-        `)
+        let data = Object.fromEntries(new FormData(e.target))
+        let observaciones= data.observaciones
+        let hora= (data.hora)
+        let tabla=document.querySelector("tbody");
+        tabla.insertAdjacentHTML("beforeend",`
+            <tr>
+                <td> ${observaciones}  </td>
+                <td> ${hora} </td>
+            </tr>
+            `)
+
     })
 })
