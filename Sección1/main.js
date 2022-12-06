a6f06ac394bd1295882dd27d6805814c11bd94bd
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-12-06 09:28:37 -0500

    Sección6

--- Sección1/main.js
+++ Sección1/main.js
@@ -1,19 +1,15 @@
 addEventListener("DOMContentLoaded", (e) =>{
-    form=document.querySelector("#form")
-        let form=document.querySelector("#form")
-    
-        form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        let data = Object.fromEntries(new FormData(e.target))
-        let observaciones= data.observaciones
-        let hora= (data.hora)
-        let tabla=document.querySelector("tbody");
-        tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-                <td> ${observaciones}  </td>
-                <td> ${hora} </td>
-            </tr>
-            `)
+    let form=document.querySelector("#form")
+    form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let ingredientes= data.ingredientes
+    let cantidad= (data.kg)
+    let tabla=document.querySelector("tbody");
+    tabla.insertAdjacentHTML("beforeend",`
+        <tr>  <td> ${ingredientes}  </td>
+        <td> ${cantidad} </td>
+    </tr>
+    `)
 
-    })
+})
 })
