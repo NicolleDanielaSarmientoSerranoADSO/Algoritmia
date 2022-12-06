ed07de6e235dba29ad0e201086216023217dba49
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-12-06 08:42:10 -0500

    Solución3

--- Sección1/main.js
+++ Sección1/main.js
@@ -1,2 +1,19 @@
-
+addEventListener("DOMContentLoaded", (e) =>{
+    form=document.querySelector("#form")
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        let data=Object.fromEntries(new FormData(e.target))
+        let dolares=Number(data.dolares)
+        let pesos=4800
+        let conver=dolares*pesos
+    
+        let mytable=document.querySelector("tbody");
+        mytable.insertAdjacentHTML("beforeend",`
+        <tr>
+            <td>${dolares}</td>
+            <td>${conver}</td>
+        </tr>
+        `)
+    })
+    })
 
